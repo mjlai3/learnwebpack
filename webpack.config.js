c8e7a83f5134736d1c6ca1c9cdacaf47a13ffdfc
Michael Lai <michaeljin.lai@gmail.com>
2017-05-09 15:02:32 +1000

    Add jquery, css loader, babel with es6 loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 var path = require('path');
 
 module.exports = {
-	entry: './app/index.js',
+	entry: './app/main.js',
 	output: {
 		filename: 'bundle.js',
-		publicPath: '/dist'
+		publicPath: '/dist/',
+		path: path.resolve(__dirname, 'dist/')
 	},
-	devtool: 'source-map'
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: [/node_modules/],
+				use: [{
+					loader: 'babel-loader',
+					options: { presets: ['es2015'] },
+				}],
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			}
+		]
+	}
 };
