7a6db85fb6c80a91b5012583a4133eb87395f553
Michael Lai <michael.lai@adelphi.digital>
2017-05-09 11:24:53 +1000

    Add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 var path = require('path');
 
 module.exports = {
-  entry: './app/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devtool: 'cheap-eval-source-map'
+	entry: './app/index.js',
+	output: {
+		filename: 'bundle.js',
+		publicPath: '/dist'
+	},
+	devtool: 'source-map'
 };
